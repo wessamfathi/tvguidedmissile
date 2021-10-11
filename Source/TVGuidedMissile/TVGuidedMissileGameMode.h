4eb8e92baf954bb60bf04bba343e7f5ca21b595c
Wessam Ibrahim <wessamfathi@gmail.com>
2021-10-11 22:20:03 +0200

    Initial project

---
+++ Source/TVGuidedMissile/TVGuidedMissileGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TVGuidedMissileGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ATVGuidedMissileGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ATVGuidedMissileGameMode();
+};
+
+
+
