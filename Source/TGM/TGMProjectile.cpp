d8f6a2b8f0546f927b67b41a86295f43a63781ac
Wessam Ibrahim <wessamfathi@gmail.com>
2021-10-30 19:10:44 +0200

    Re-add the tv-guided missile code and blueprint

--- Source/TGM/TGMProjectile.cpp
+++ Source/TGM/TGMProjectile.cpp
@@ -1,43 +1,216 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
 #include "TGMProjectile.h"
-#include "GameFramework/ProjectileMovementComponent.h"
-#include "Components/SphereComponent.h"
+#include "Camera/CameraComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "Particles/ParticleSystemComponent.h"
 
-ATGMProjectile::ATGMProjectile() 
+// Sets default values
+ATGMProjectile::ATGMProjectile()
 {
-	// Use a sphere as a simple collision representation
-	CollisionComp = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComp"));
-	CollisionComp->InitSphereRadius(5.0f);
-	CollisionComp->BodyInstance.SetCollisionProfileName("Projectile");
-	CollisionComp->OnComponentHit.AddDynamic(this, &ATGMProjectile::OnHit);		// set up a notification for when this component hits something blocking
+ 	// Set this actor to call Tick() every frame
+	PrimaryActorTick.bCanEverTick = true;
 
-	// Players can't walk on it
-	CollisionComp->SetWalkableSlopeOverride(FWalkableSlopeOverride(WalkableSlope_Unwalkable, 0.f));
-	CollisionComp->CanCharacterStepUpOn = ECB_No;
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("ProjectileSceneComponent"));
 
-	// Set as root component
-	RootComponent = CollisionComp;
+	// Use a sphere as a simple collision representation.
+	CollisionComponent = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComponent"));
+	// Set the sphere's collision radius.
+	CollisionComponent->InitSphereRadius(15.0f);
+	// Set the root component to be the collision component.
+	RootComponent = CollisionComponent;
 
-	// Use a ProjectileMovementComponent to govern this projectile's movement
-	ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileComp"));
-	ProjectileMovement->UpdatedComponent = CollisionComp;
-	ProjectileMovement->InitialSpeed = 3000.f;
-	ProjectileMovement->MaxSpeed = 3000.f;
-	ProjectileMovement->bRotationFollowsVelocity = true;
-	ProjectileMovement->bShouldBounce = true;
+	// Use this component to drive this projectile's movement.
+	ProjectileMovementComponent = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileMovementComponent"));
+	ProjectileMovementComponent->SetUpdatedComponent(CollisionComponent);
+	ProjectileMovementComponent->InitialSpeed = 3000.0f;
+	ProjectileMovementComponent->MaxSpeed = 3000.0f;
+	ProjectileMovementComponent->bRotationFollowsVelocity = false;
+	ProjectileMovementComponent->bShouldBounce = true;
+	ProjectileMovementComponent->Bounciness = 0.3f;
+	ProjectileMovementComponent->ProjectileGravityScale = 0.0f;
 
-	// Die after 3 seconds by default
-	InitialLifeSpan = 3.0f;
+	// Create the static mesh component for this projectile
+	ProjectileMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ProjectileMeshComponent"));
+
+	if (ProjectileMeshComponent != nullptr)
+	{
+		ProjectileMeshComponent->SetStaticMesh(ProjectileMesh);
+		ProjectileMeshComponent->SetMaterial(0, ProjectileMaterial);
+	}
+
+	// Create a follow camera with special VFX
+	ProjectileCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	ProjectileCamera->SetupAttachment(RootComponent); // Attach the camera to character's root component
+	ProjectileCamera->bUsePawnControlRotation = true; // Camera follows pawn controller rotation
+	ProjectileCamera->PostProcessSettings.ColorSaturation = FVector4(0, 0, 0, 0);
+	ProjectileCamera->PostProcessSettings.bOverride_ColorSaturation = true;
+	ProjectileCamera->PostProcessSettings.GrainIntensity = 0.6f;
+	ProjectileCamera->PostProcessSettings.bOverride_GrainIntensity = true;
+	ProjectileCamera->PostProcessSettings.bOverride_GrainJitter = true;
+	ProjectileCamera->PostProcessSettings.bOverride_VignetteIntensity = true;
+	ProjectileCamera->PostProcessSettings.VignetteIntensity = 0.8f;
+	ProjectileCamera->PostProcessSettings.GrainJitter = 1.0f;
+
+	// Projectile should self-destruct after set time
+	ProjectileLifeSpan = 5.0f;
+
+	// Set the projectile's collision profile
+	CollisionComponent->BodyInstance.SetCollisionProfileName(TEXT("Projectile"));
+
+	// Callback when the projectile hits something
+	CollisionComponent->OnComponentHit.AddDynamic(this, &ATGMProjectile::OnHit);
+
+	// Set initial input related values
+	BaseTurnRate = 1.0f;
+	BaseLookUpRate = 1.0f;
+	TurnRateMultiplier = 0.03f;
+	LookUpRateMultiplier = 0.03f;
+
+	// Input modifiers used when player boosts the projectile
+	BoostHandlingMultiplier = 0.333f;
+	BoostSpeedMultiplier = 2.0f;
+	bIsBoosted = false;
+
+	// Explosion related values
+	ImpulseRadius = 300.0f;
+	ImpulseMagnitude = 500000.0f;
 }
 
-void ATGMProjectile::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
-{
-	// Only add impulse and destroy projectile if we hit a physics
-	if ((OtherActor != nullptr) && (OtherActor != this) && (OtherComp != nullptr) && OtherComp->IsSimulatingPhysics())
-	{
-		OtherComp->AddImpulseAtLocation(GetVelocity() * 100.0f, GetActorLocation());
+//////////////////////////////////////////////////////////////////////////
+// Input
 
-		Destroy();
+void ATGMProjectile::SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent)
+{
+	// Set up gameplay key bindings
+	check(PlayerInputComponent);
+
+	// We have 2 versions of the rotation bindings to handle different kinds of devices differently
+	// "turn" handles devices that provide an absolute delta, such as a mouse.
+	// "turnrate" is for devices that we choose to treat as a rate of change, such as an analog joystick
+	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("TurnRate", this, &ATGMProjectile::TurnAtRate);
+	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
+	PlayerInputComponent->BindAxis("LookUpRate", this, &ATGMProjectile::LookUpAtRate);
+
+	// Custom projectile input controls
+	PlayerInputComponent->BindAction("Explode", IE_Pressed, this, &ATGMProjectile::Explode);
+	PlayerInputComponent->BindAction("Boost", IE_Pressed, this, &ATGMProjectile::Boost);
+}
+
+void ATGMProjectile::TurnAtRate(float Rate)
+{
+	// calculate delta for this frame from the rate information
+	AddControllerYawInput(Rate * BaseTurnRate * GetWorld()->GetDeltaSeconds());
+}
+
+void ATGMProjectile::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// Set a timer to explode the projectile after its lifespan is over
+	FTimerHandle TimerHandle;
+	GetWorld()->GetTimerManager().SetTimer(TimerHandle, this, &ATGMProjectile::Explode, ProjectileLifeSpan, false);
+}
+
+void ATGMProjectile::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	// Set the movement component velocity manually every frame so that it follows the rotation
+	ProjectileMovementComponent->Velocity = Controller->GetControlRotation().Vector() * ProjectileMovementComponent->MaxSpeed;
+}
+
+
+void ATGMProjectile::AddControllerYawInput(float Val)
+{
+	// Use a custom TurnRateMultiplier to limit handling
+	Val = Val * TurnRateMultiplier;
+	Super::AddControllerYawInput(Val);
+}
+
+
+void ATGMProjectile::LookUpAtRate(float Rate)
+{
+	// calculate delta for this frame from the rate information
+	AddControllerPitchInput(Rate * BaseLookUpRate * GetWorld()->GetDeltaSeconds());
+}
+
+void ATGMProjectile::AddControllerPitchInput(float Val)
+{
+	// Use a custom LookUpRateMultiplier to limit handling
+	Val = Val * LookUpRateMultiplier;
+	Super::AddControllerPitchInput(Val);
+}
+
+void ATGMProjectile::FireInDirection(const FVector& ShootDirection, APawn* pawnOwner)
+{
+	ProjectileMovementComponent->Velocity = ShootDirection * ProjectileMovementComponent->InitialSpeed;
+	ProjectileCamera->SetActive(true);
+	PawnOwner = pawnOwner;
+}
+
+void ATGMProjectile::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit)
+{
+	Explode();
+}
+
+void ATGMProjectile::Explode()
+{
+	// Spawn and activate explosion VFX
+	UParticleSystemComponent* PSC = UGameplayStatics::SpawnEmitterAtLocation(this, ExplosionFX, GetActorLocation(), GetActorRotation(), true);
+	PSC->ActivateSystem();
+
+	// Simulate projectile shockwave
+	ApplyRadialImpulse();
+
+	// Let the controller possess the original character
+	if (Controller != nullptr)
+	{
+		Controller->Possess(PawnOwner);
+	}
+
+	// Finally projectile should be destroyed
+	Destroy();
+}
+
+void ATGMProjectile::ApplyRadialImpulse()
+{
+	TArray<TEnumAsByte<EObjectTypeQuery>> ObjectTypes;
+	ObjectTypes.Add(UEngineTypes::ConvertToObjectType(ECollisionChannel::ECC_WorldDynamic));
+	ObjectTypes.Add(UEngineTypes::ConvertToObjectType(ECollisionChannel::ECC_PhysicsBody));
+
+	TArray<AActor*> ActorsToIgnore;
+	ActorsToIgnore.Add(this);
+
+	// Get all components within ImpulseRadius from explosion center
+	FVector ActorLocation = GetActorLocation();
+	TArray<UPrimitiveComponent*> OutComponents;
+	UKismetSystemLibrary::SphereOverlapComponents(this, ActorLocation, ImpulseRadius, ObjectTypes, nullptr, ActorsToIgnore, OutComponents);
+
+	FVector Impulse;
+	UPrimitiveComponent* Component;
+	for (int32 i = 0; i < OutComponents.Num(); i++)
+	{
+		Component = OutComponents[i];
+
+		// Add impulse from explosion center to component location
+		Impulse = (Component->GetComponentLocation() - ActorLocation).GetSafeNormal() * ImpulseMagnitude;
+		OutComponents[i]->AddImpulse(Impulse);
+	}
+}
+
+void ATGMProjectile::Boost()
+{
+	// Projectile can only be boosted once
+	if (!bIsBoosted)
+	{
+		bIsBoosted = true;
+
+		// Limit handling even more
+		LookUpRateMultiplier *= BoostHandlingMultiplier;
+		TurnRateMultiplier *= BoostHandlingMultiplier;
+
+		// Increase projectile velocity and max speed by boost factor
+		ProjectileMovementComponent->Velocity *= BoostSpeedMultiplier;
+		ProjectileMovementComponent->MaxSpeed *= BoostSpeedMultiplier;
 	}
 }
--- Source/TGM/TGMProjectile.h
+++ Source/TGM/TGMProjectile.h
@@ -1,37 +1,141 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+#include "Components/SphereComponent.h"
 #include "TGMProjectile.generated.h"
 
-class USphereComponent;
-class UProjectileMovementComponent;
-
-UCLASS(config=Game)
-class ATGMProjectile : public AActor
+/**
+ * Custom projectile class that can be steered by player
+ */
+UCLASS()
+class TGM_API ATGMProjectile : public APawn
 {
 	GENERATED_BODY()
-
-	/** Sphere collision component */
-	UPROPERTY(VisibleDefaultsOnly, Category=Projectile)
-	USphereComponent* CollisionComp;
-
-	/** Projectile movement component */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
-	UProjectileMovementComponent* ProjectileMovement;
-
+	
 public:
+
+	// Sets default values for this actor's properties
 	ATGMProjectile();
 
-	/** called when projectile hits something */
+	/**
+	 * Add input (affecting Pitch) to the Controller's ControlRotation, if it is a local PlayerController.
+	 * This value is multiplied by the PlayerController's InputPitchScale value.
+	 * @param Val Amount to add to Pitch. This value is multiplied by the PlayerController's InputPitchScale value.
+	 * @see PlayerController::InputPitchScale
+	 */
+	virtual void AddControllerPitchInput(float Val) override;
+
+	/**
+	 * Add input (affecting Yaw) to the Controller's ControlRotation, if it is a local PlayerController.
+	 * This value is multiplied by the PlayerController's InputYawScale value.
+	 * @param Val Amount to add to Yaw. This value is multiplied by the PlayerController's InputYawScale value.
+	 * @see PlayerController::InputYawScale
+	 */
+	virtual void AddControllerYawInput(float Val) override;
+
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void BeginPlay() override;
+
+	// Sphere collision component
+	UPROPERTY(VisibleDefaultsOnly, Category = Projectile)
+		USphereComponent* CollisionComponent;
+
+	// Projectile movement component.
+	UPROPERTY(VisibleAnywhere, Category = Movement)
+	UProjectileMovementComponent* ProjectileMovementComponent;
+
+	UPROPERTY()
+	UStaticMeshComponent* ProjectileMeshComponent;
+
+	UPROPERTY(EditDefaultsOnly, Category = Projectile)
+	UStaticMesh* ProjectileMesh;
+
+	UPROPERTY(EditDefaultsOnly, Category = Projectile)
+	UMaterial* ProjectileMaterial;
+
+	// Function that initializes the projectile's velocity in the shoot direction.
+	void FireInDirection(const FVector& ShootDirection, class APawn* pawnOwner);
+
+protected:
+	
+	// Follow camera
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class UCameraComponent* ProjectileCamera;
+
+	UPROPERTY()
+	class APawn* PawnOwner;
+
+	UPROPERTY(EditDefaultsOnly)
+	class UParticleSystem* ExplosionFX;
+
+	// Base turn rate, in deg/sec. Other scaling may affect final turn rate.
+	UPROPERTY(EditDefaultsOnly, Category = Handling)
+	float BaseTurnRate;
+
+	// Base look up/down rate, in deg/sec. Other scaling may affect final rate.
+	UPROPERTY(EditDefaultsOnly, Category = Handling)
+	float BaseLookUpRate;
+
+	// Turn rate multiplier to limit handling
+	UPROPERTY(EditDefaultsOnly, Category = Handling)
+	float TurnRateMultiplier;
+
+	// Lookup rate multiplier to limit handling
+	UPROPERTY(EditDefaultsOnly, Category = Handling)
+	float LookUpRateMultiplier;
+
+	// Handling multiplier used when projectile is boosted
+	UPROPERTY(EditDefaultsOnly, Category = Handling)
+	float BoostHandlingMultiplier;
+
+	// Speed multiplier used when projectile is boosted
+	UPROPERTY(EditDefaultsOnly, Category = Movement)
+	float BoostSpeedMultiplier;
+
+	// Time after which projectile self-destructs
+	UPROPERTY(EditDefaultsOnly, Category = Projectile)
+	float ProjectileLifeSpan;
+
+	// Radius within explosion impulse is used
+	UPROPERTY(EditDefaultsOnly, Category = Projectile)
+	float ImpulseRadius;
+
+	// Explosion impulse value
+	UPROPERTY(EditDefaultsOnly, Category = Projectile)
+	float ImpulseMagnitude;
+
+	// Whether projectile has already been boosted
+	bool bIsBoosted;
+
+	// Called when the projectile hits something
 	UFUNCTION()
-	void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+	void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit);
 
-	/** Returns CollisionComp subobject **/
-	USphereComponent* GetCollisionComp() const { return CollisionComp; }
-	/** Returns ProjectileMovement subobject **/
-	UProjectileMovementComponent* GetProjectileMovement() const { return ProjectileMovement; }
+	// Simulate explosion shockwave
+	void ApplyRadialImpulse();
+
+	/**
+	 * Called via input to turn at a given rate.
+	 * @param Rate	This is a normalized rate, i.e. 1.0 means 100% of desired turn rate
+	 */
+	void TurnAtRate(float Rate);
+
+	/**
+	 * Called via input to turn look up/down at a given rate.
+	 * @param Rate	This is a normalized rate, i.e. 1.0 means 100% of desired turn rate
+	 */
+	void LookUpAtRate(float Rate);
+
+	// APawn interface
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	// End of APawn interface
+
+	// Explode the projectile and play all relevant FX
+	void Explode();
+
+	// Boost projectile speed on player input
+	void Boost();
 };
-
